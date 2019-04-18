1cff28a9a82d0cd7c784890fe6488bd7123e36f5
Felix Mutugi <stunnerszone@gmail.com>
2019-04-18 21:14:16 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let http = require('http');
+let bodyParser =require('body-parser');
+let path =require('path');
+let express = require('express');
+const pug =require('pug');
+let lookup=require('./js/whois');
+let format =require('./js/domain_format');
+
+
+
+let app = express();
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({ extended: true })); 
+// app.use(express.static(__dirname +'/public'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/views/index.html');
+});
+
+app.post('/api',function(req,res){
+
+	let domain = req.body.value;
+
+    if(format.check(domain)){
+        lookup.initConn(domain,res);
+	}else{
+		res.render('error');
+	}
+
+});
+
+http.createServer(app)
+    .listen(4500);
+
+console.log('Server Started....');       
+   
